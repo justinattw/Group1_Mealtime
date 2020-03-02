fb9500f6f040d5d8f9c44a7d0ce5d7971242c6c9
justinattw <justinattw@icloud.com>
2020-03-02 16:46:27 +0000

    Wrote a fake test

--- test/backend_tests.py
+++ test/backend_tests.py
@@ -5,5 +5,9 @@ from flask_testing import TestCase
 
 class TestMain():
 
+    # Fake test that passes
     def test_psuedo(self):
         return True
+
+if __name__ == '__main__':
+    unittest.main()
